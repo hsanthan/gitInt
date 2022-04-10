41f58c3cfb4351176bfb70a8aa75d409b50abce1
Hema <71802450+hsanthan@users.noreply.github.com>
2022-04-10 09:00:57 -0300

    new test proj

---
+++ main.py
@@ -0,0 +1,13 @@
+from sampleimp import SampleImp as SI
+
+
+if __name__ == '__main__':
+    new_si = SI()
+    new_si.printmsg()
+
+
+
+
+
+
+
---
+++ sampleimp.py
@@ -0,0 +1,8 @@
+import os,sys
+
+class SampleImp:
+    def __init__(self):
+        pass
+
+    def printmsg(self):
+        print('hey there...from the import sample')
